b23e4e583112ba1c06756b72145671b16e8b1aee
Jose Avalos <luizavalos40@gmail.com>
2025-04-15 18:51:05 -0500

    update test to use MMA link

--- cypress/e2e/sportsbookodds.cy.ts
+++ cypress/e2e/sportsbookodds.cy.ts
@@ -7,13 +7,8 @@ describe("Sportsbook Odds", () => {
   it("links load up correct page", () => {
     cy.visit("/");
     cy.get('[data-cy="bars-icon"]').click();
-    cy.get('[data-cy="nfl-link"]').click();
-    cy.url().should("include", "/americanfootball_nfl");
-
-    //TODO! Find a way to run this test only when NBA season/preaseason starts
-    // cy.get('[data-cy="bars-icon"]').click();
-    // cy.get('[data-cy="nba-link"]').click();
-    // cy.url().should("include", "/basketball_nba");
+    cy.get('[data-cy="mma-link"]').click();
+    cy.url().should("include", "/mma_mixed_martial_arts");
   });
 });
 
