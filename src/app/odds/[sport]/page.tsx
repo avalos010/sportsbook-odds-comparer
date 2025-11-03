1a15b7b2b94994c1e6b11caa8a8048599a671548
Jose Avalos <luizavalos40@gmail.com>
2025-11-02 20:54:51 -0600

    fix: remove params import

--- src/app/odds/[sport]/page.tsx
+++ src/app/odds/[sport]/page.tsx
@@ -1,8 +1,8 @@
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { redirect } from "next/navigation";
 
-const Page = async ({ params }: { params: Params }) => {
-  const { sport } = params;
+const Page = async ({ params }: { params: Promise<{ sport: string }> }) => {
+  const resolvedParams = await params;
+  const { sport } = resolvedParams;
 
   return redirect(`odds/${sport}/moneyline`); //redirect to the moneyline page by default
 };
